0d57a0ebdf10e0f6943bdfb35616bdb730a5a176
idanfath <arfounded@gmail.com>
2024-05-14 20:43:18 +0800

    qol and docs to reduce lines

--- attempt 1/app.js
+++ attempt 1/app.js
@@ -1,77 +1,76 @@
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext("2d")
-const popup = document.getElementById("popup")
-const popuptop = document.getElementById("popuptop")
 
-// [1] ease of calling
-const canvaswidth = canvas.width;
-const canvasheight = canvas.height;
 
-// [2] clogging debug function
-function debug() { ifmainmenu() }
 
-// player attribute
+//attributes
 let lives = 3,
-    score = 0,
+	score = 0,
 	play = 0,
 	highscore = localStorage.getItem('highscore')
-// game attribute
-let ballx = canvaswidth,
-    bally = canvasheight - 100,
-    ballsize = 10,
-    ballspdx = -2,
-    ballspdy = 2
+let ballx = canvas.width / 2,
+	bally = canvas.height - 100,
+	ballsize = 10,
+	ballspdx = -2,
+	ballspdy = -2
 let paddlewidth = 200,
-    paddleheight = 20,
-    paddlex = canvaswidth / 2 - paddlewidth / 2,
-    paddley = canvasheight - paddleheight - 40,
-    paddlespd = 10,
-    toright = false,
-    toleft = false
+	paddleheight = 20,
+	paddlex = canvas.width / 2 - paddlewidth / 2,
+	paddley = canvas.height - paddleheight,
+	paddlespd = 10,
+	toright = false,
+	toleft = false
 let brickwidth = 85,
-    brickheight = 40,
-    bricks = []
+	brickheight = 40,
+	bricks = []
 
 // generating bricks with rows (y), and columns (x)
 for (let y = 0; y < 6; y++) {
-    for (let x = 0; x < 7; x++) {
-        let brick = {
-            // level = using ternary to set level rarity
-            level: Math.random() <= 0.1 ? 1 : Math.random() <= 0.3 ? 2 : 3,
-            x: x * (brickwidth + 10) + 20,
-            y: y * (brickheight + 20) + 40,
-        }
-        bricks.push(brick)
-    }
+	for (let x = 0; x < 7; x++) {
+		let brick = {
+			// level = using ternary to set level rarity
+			// level: Math.random() <= 0.1 ? 1 : Math.random() <= 0.3 ? 2 : 3,
+			level: 1,
+			x: x * (brickwidth + 10) + 20,
+			y: y * (brickheight + 20) + 40,
+		}
+		bricks.push(brick)
+	}
 }
 
 // game utils functions for qol
+const popup = document.getElementById("popup")
 function up(content = '', color, fontsize = "30px") {
-    popup.innerHTML +=
-        `<div style="color: ${color}; font-size: ${fontsize};"> ${content}</div>`
+	popup.innerHTML += `<div style="color: ${color}; font-size: ${fontsize};"> ${content}</div>`
 }
+function upbtn(content = '', att) {
+	popup.innerHTML += `<button onclick="${att}"> ${content} </button>`
+}
+
+const popuptop = document.getElementById("popuptop")
 function upboard(content = '') {
-    popuptop.innerHTML =
-        `<div> ${content} </div>`
-}
-function upbtn(content = '', att){
-    popup.innerHTML += 
-        `<button onclick="${att}"> ${content} </button>`
+	popuptop.innerHTML = `<div> ${content} </div>`
 }
 function down() {
-    popup.innerHTML = ''
+	popup.innerHTML = ''
 	popuptop.innerHTML = ''
 }
-function makebrick(x, y, color = 'white'){
-    context.beginPath();
-	context.rect(x, y, brickwidth, brickheight);
+
+function draw(type, color, x,y, a,b,c){
+	if (!type) {return}
+	context.beginPath();
+	switch (type) {
+		case 'rect': context.rect(x, y, a, b); break;
+		case 'arc': context.arc(x, y, a, b, c); break;
+		case 'rect': context.rect(x, y, a, b); break;
+	}
 	context.fillStyle = color;
 	context.fill();
 	context.closePath();
 }
 
 let gamestate
-function initplay(){
+function initplay() {
 	down()
 	play = 1
 	gamestate = setInterval(rendergame, 10);
@@ -79,76 +78,51 @@ function initplay(){
 
 // handle game state popup
 const ifgameover = () => {
-    down()
-	if (score > highscore) {
-		localStorage.setItem('highscore', score)
-		up('New Highscore!!')
-	}
-    up(
-		`Game Over!! <br> 
-        Score: ${score}`,
-        'red'
-    )
-    upbtn('play again', 'window.location.reload()')
-    upbtn('close window', 'window.close()')
+	down()
+	if (score > highscore) {localStorage.setItem('highscore', score); up('New Highscore!!')}
+	up(`Game Over!! <br> Score: ${score}`, 'red')
+	upbtn('play again', 'window.location.reload()')
+	upbtn('close window', 'window.close()')
 }
 const ifgamefinish = () => {
-    down()
-    up(`You Won!! <br> Score: ${score}`, 'lightgreen')
-    upbtn('play again', 'window.location.reload()')
-    upbtn('close window', 'window.close()')
-	
+	if (score > highscore) {localStorage.setItem('highscore', score); up('New Highscore!!')}
+	down()
+	up(`You Won!! <br> Score: ${score}`, 'lightgreen')
+	upbtn('play again', 'window.location.reload()')
+	upbtn('close window', 'window.close()')
 }
 const ifmainmenu = () => {
 	down()
-	if (highscore > 0) {
-		upboard(`Highscore: ${highscore}`)
-	}
-    up('My Test Game')
-    upbtn('play now', 'initplay(1);')
-    upbtn('close window', 'window.close()')
+	if (highscore > 0) {upboard(`Highscore: ${highscore}`)}
+	up('My Test Game')
+	upbtn('play now', 'initplay(1);')
+	upbtn('close window', 'window.close()')
 }
 const scores = () => {
-	showhighscore = highscore > 0 ? `Highscore: ${highscore}` : '';
-	upboard(`Score: ${score} <br> Lives: ${lives} <br><br>` + showhighscore )
+	upboard(`Score: ${score} <br> Lives: ${lives} <br><br>` + (highscore > 0 ? `Highscore: ${highscore}` : ''))
 }
 
 
 const renderbrick = () => {
-    bricks.forEach((e) => {
-        if (e.level == 1) {
-			makebrick(e.x, e.y, 'red');
+	bricks.forEach((e) => {
+		let color;
+		switch (e.level) {
+			case 1: color = 'red'; break
+			case 2: color = 'yellow'; break
+			case 3: color = 'green'; break
 		}
-		if (e.level == 2) {
-			makebrick(e.x, e.y, 'yellow');
-		}
-		if (e.level == 3) {
-			makebrick(e.x, e.y, 'green');
-		}
-    });
+		if (color){draw('rect', color, e.x, e.y, brickwidth, brickheight)}
+	});
 }
 
 const renderpaddle = () => {
-	context.beginPath();
-	context.rect(paddlex, paddley, paddlewidth, paddleheight);
-	context.fillStyle = "skyblue";
-	context.fill();
-	context.closePath();
-
-	if (toleft == true && paddlex >= 0) {
-		paddlex -= paddlespd;
-	}
-	if (toright == true && paddlex <= canvaswidth - paddlewidth) {
-		paddlex += paddlespd;
-	}
+	draw('rect', 'skyblue', paddlex, paddley, paddlewidth, paddleheight)
+	if (toleft && paddlex >= 0) {paddlex -= paddlespd}
+	if (toright && paddlex <= canvas.width - paddlewidth) {paddlex += paddlespd}
 };
 
 const renderball = () => {
-	context.beginPath();
-	context.arc(ballx, bally, ballsize, 0, 3 + Math.PI);
-	context.fillStyle = "purple";
-	context.fill();
-	context.closePath();
+	draw('arc', 'white', ballx, bally, ballsize, 0, 6)
 	handlewhileplay();
 };
 
@@ -156,19 +130,17 @@ const handlewhileplay = () => {
 	ballx += ballspdx;
 	bally += ballspdy;
 
-	if (ballx >= canvaswidth - ballsize) {
-		ballspdx = -ballspdx;
-	}
+	//these two process validate wether the ball touches left/top side of canvas
+	//its simpler than right/bottom since we know that the wall position is 0 
+	//check if ball position is less than ball size, then reverse the momentum
+	if (ballx <= ballsize) {ballspdx = -ballspdx;}
+	if (bally <= ballsize) {ballspdy = -ballspdy;}
 
-	if (ballx <= ballsize) {
-		ballspdx = -ballspdx;
-	}
-
-	if (bally <= ballsize) {
-		ballspdy = -ballspdy;
-	}
-
-	if (bally >= canvasheight - ballsize - paddleheight) {
+	//these two are a bit complicated as they check for right wall and bottom wall
+	//taking into account ball-paddle interaction
+	//!continue here
+	if (ballx >= canvas.width - ballsize) {ballspdx = -ballspdx;}
+	if (bally >= canvas.height - ballsize - paddleheight) {
 		if (
 			ballx + ballsize >= paddlex &&
 			ballx + ballsize <= paddlex + paddlewidth
@@ -176,12 +148,12 @@ const handlewhileplay = () => {
 			ballspdy = -ballspdy;
 		} else {
 			lives -= 1;
-			ballx = canvaswidth / 2;
-			bally = canvasheight / 2;
+			ballx = canvas.width / 2;
+			bally = canvas.height / 2;
 			ballspdx = -2;
 			ballspdy = -2;
-			paddlex = canvaswidth / 2 - paddlewidth / 2;
-			paddley = canvasheight - paddleheight - 40;
+			paddlex = canvas.width / 2 - paddlewidth / 2;
+			paddley = canvas.height - paddleheight;
 		}
 	}
 
@@ -204,21 +176,15 @@ const handlewhileplay = () => {
 	});
 };
 
-const keydown = (e) => {
-	if (e.keyCode == 37) {toleft = true} else if (e.keyCode == 39) {toright = true}
+const keyhandler = (e, state) => {
+	e.keyCode == 37 ? toleft = state : e.keyCode == 39 ? toright = state : null
 };
 
 const keyup = (e) => {
-	if (e.keyCode == 37) {
-		toleft = false;
-	}
-	if (e.keyCode == 39) {
-		toright = false;
-	}
 };
 
 const rendergame = () => {
-	context.clearRect(0, 0, canvaswidth, canvasheight);
+	context.clearRect(0, 0, canvas.width, canvas.height);
 
 	let win = true;
 
@@ -234,7 +200,7 @@ const rendergame = () => {
 	} else {
 		if (win) {
 			ifgamefinish();
-		} else if (play == 0){
+		} else if (play == 0) {
 			ifmainmenu()
 		} else {
 			scores()
@@ -245,8 +211,8 @@ const rendergame = () => {
 	}
 };
 
-document.addEventListener("keydown", keydown);
-document.addEventListener("keyup", keyup);
+document.addEventListener("keydown", (e) => keyhandler(e, true));
+document.addEventListener("keyup", (e) => keyhandler(e, false));
 
 
 rendergame()
